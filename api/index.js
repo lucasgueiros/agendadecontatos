1d8076cf539ceaff10075bf55f876f0d3cf2e99b
Lucas Dantas Gueiros <lucasdantasgueiros@gmail.com>
2021-09-16 13:59:53 -0300

    criando tabela contatos

--- api/index.js
+++ api/index.js
@@ -11,3 +11,32 @@ app.use(express.json());
 app.get('/', (req, res) => res.json({ message: 'Hello World' }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// NOME, SOBRENOME, TELEFONE, DATA DE NASCIMENTO, ENDERECO e EMAIL
+const Contato = sequelize.define('contato', {
+	nome: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	sobrenome: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	telefone: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	dataDeNascimento:{
+		type: Sequelize.DATEONLY,
+		allowNull: false
+	},
+	endereco:{
+		type: Sequelize.STRING,
+		allowNull: false
+	}, 
+	email:{
+		type: Sequelize.STRING,
+		allowNull: false
+	}
+});
+Contato.sync({ force: true });
