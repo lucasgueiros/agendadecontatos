a86bf0ad1e16f28979b1beb05d7ec95de64a2b14
Lucas Dantas Gueiros <lucasdantasgueiros@gmail.com>
2021-09-16 13:51:29 -0300

    conexao com postgresql local funcionando

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('postgres://usuario:senha@localhost:5432/sereducacional')
 
 const app = express()
 
@@ -9,4 +11,3 @@ app.use(express.json());
 app.get('/', (req, res) => res.json({ message: 'Hello World' }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
---
+++ postgre.sql
@@ -0,0 +1,2 @@
+create user usuario with password 'senha';
+create database sereducacional with owner usuario;
