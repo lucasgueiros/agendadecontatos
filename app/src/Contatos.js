f8e53c81ec642989612a94629ed87d1673e059a3
Lucas Dantas Gueiros <lucasdantasgueiros@gmail.com>
2021-09-17 10:47:17 -0300

    operação remover quase boa, falta o fetching

--- app/src/Contatos.js
+++ app/src/Contatos.js
@@ -1,24 +1,47 @@
 import {useAuth} from './Auth.js';
 import {useState, useEffect} from 'react';
-import axios from 'axios';
+import {useRestResource} from './generics';
+import {Button, Card, CardColumns, Collapse} from 'react-bootstrap';
 
 export const Contatos = (props) => {
   const [auth] = useAuth();
-  const [data, setData] = useState();
+  const [data, fetch, remove] = useRestResource('contatos', auth.config);
 
-  useEffect(() => {
-    axios.get('/contatos', auth.config).then(
-      (r) => {
-        setData(r.data);
-      }, (e) => {
-        console.log(e);
-      }
-    )
-  }, [auth])
-
+  if(!data) {
+    return <>Carregando</>
+  }
   return (
+    <>
     <h1>Contatos</h1>
+    <CardColumns>
+      {
+        data.map((contato) => <ContatoCard
+          contato={contato}/>)
+      }
 
-
+    </CardColumns>
+    </>
   );
 };
+
+// NOME, SOBRENOME, TELEFONE, DATA DE NASCIMENTO, ENDERECO e EMAIL
+const ContatoCard = ({contato, remove}) => {
+  const [open, setOpen] = useState(false);
+  return <>
+    <Card onClick={() => setOpen(!open)} body>
+      <Card.Title>{contato.nome + ' ' + contato.sobrenome}</Card.Title>
+      <Collapse  in={open}>
+        <div>
+          Telefone: {contato.telefone}<br/>
+          Data de nascimento: {contato.dataDeNascimento}<br/>
+          Endereço: {contato.endereco}<br/>
+          E-mail: {contato.email}<br/>
+          <br/>
+          <Button varian="primary">Editar</Button>{' '}
+          <Button varian="primary"
+            onClick={(e) => {contato._delete()}}>Excluir</Button>
+        </div>
+      </Collapse>
+    </Card>
+  </>;
+}
--- app/src/generics.js
+++ app/src/generics.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 // from https://dev.to/selbekk/persisting-your-react-state-in-9-lines-of-code-9go
 export function usePersistedState(key, defaultValue) {
@@ -9,4 +10,29 @@ export function usePersistedState(key, defaultValue) {
     localStorage.setItem(key, JSON.stringify(state));
   }, [key, state]);
   return [state, setState];
+}
+
+function fetch (name, config, setData) {
+  axios.get('/' + name, config).then(
+    (r) => {
+      setData(r.data[name].map((d) => {
+        return {
+          ...d,
+          _delete: async () => {
+            await axios.delete('/' + name + '/' + d.id, config);
+            fetch();
+          }
+        }
+      }));
+    }, (e) => {
+      console.log(e);
+    }
+  )
+}
+
+export function useRestResource(name, config) {
+  const [data, setData] = useState();
+
+  useEffect(() => fetch(name, config, setData), [config, name]);
+  return [data, fetch];
 }
