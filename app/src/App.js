e2a05a5b1608e6cfb70295317860fd3eeadd562f
Lucas Dantas Gueiros <lucasdantasgueiros@gmail.com>
2021-09-18 19:53:19 -0300

    melhorias no visual

--- app/src/App.js
+++ app/src/App.js
@@ -10,34 +10,34 @@ import {
   Link
 } from "react-router-dom";
 
-
 const App = () => {
   return (
       <Router>
-        <Navbar bg="light" expand="lg">
-          <Container>
-            <Navbar.Brand as={Link} to="/">Agenda de contatos</Navbar.Brand>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                <Nav.Link as={Link} to="/contatos">Contatos</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-          <Navbar.Collapse className="justify-content-end">
-            <Nav className="me-auto">
-            <Auth/>
-          </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Switch>
-          <Route path="/contatos">
-            <Contatos/>
-          </Route>
-          <Route path="/">
-            <h1>Welcome</h1>
-          </Route>
-        </Switch>
+        <div className="container">
+          <Navbar bg="primary" variant="dark" expand="sm">
+            <Container>
+              <Navbar.Brand as={Link} to="/">Agenda de contatos</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/">Home</Nav.Link>
+                  <Nav.Link as={Link} to="/contatos">Contatos</Nav.Link>
+                </Nav>
+                <Nav>
+                  <Auth/>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          <Switch>
+            <Route path="/contatos">
+              <Contatos/>
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
       </Router>
 
   );
